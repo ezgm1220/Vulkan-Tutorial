50b30b6c65b7d05903199435211f08e4ebbca481
ezgm1220 <2431510525@qq.com>
2023-12-05 10:33:30 +0800

    Setup_Validation layers

--- DrawTriangle/include/HelloTriangleApplication.h
+++ DrawTriangle/include/HelloTriangleApplication.h
@@ -3,6 +3,15 @@
 #include <GLFW/glfw3.h>
 #include <vector>
 
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
 class HelloTriangleApplication
 {
 public:
@@ -18,6 +27,7 @@ private:
     GLFWwindow* window;
 
     VkInstance instance;
+    VkDebugUtilsMessengerEXT debugMessenger;
 
     void initWindow();
 
@@ -30,4 +40,18 @@ private:
     void createInstance(); // 创建实例
 
     void PrintExtension(); // 打印我们可用的扩展
+
+    bool checkValidationLayerSupport();// 检查所有请求的层是否可用
+
+    std::vector<const char*> getRequiredExtensions();// 获取我们所需的扩展列表
+
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+        VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+        VkDebugUtilsMessageTypeFlagsEXT messageType,
+        const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+        void* pUserData);// debug的回调函数
+
+    void setupDebugMessenger();
+
+    void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);// 填充VkDebugUtilsMessengerCreateInfoEXT的信息
 };
--- DrawTriangle/src/HelloTriangleApplication.cpp
+++ DrawTriangle/src/HelloTriangleApplication.cpp
@@ -7,6 +7,34 @@
 extern const uint32_t WIDTH;
 extern const uint32_t HEIGHT;
 
+
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, 
+                                      const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, 
+                                      const VkAllocationCallbacks* pAllocator, 
+                                      VkDebugUtilsMessengerEXT* pDebugMessenger)
+{// 通过vkCreateDebugUtilsMessengerEXT方法创建VkDebugUtilsMessengerEXT对象
+    auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+    if(func != nullptr)
+    {
+        return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+    }
+    else
+    {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+void DestroyDebugUtilsMessengerEXT(VkInstance instance, 
+                                   VkDebugUtilsMessengerEXT debugMessenger, 
+                                   const VkAllocationCallbacks* pAllocator)
+{// 清理VkDebugUtilsMessengerEXT
+    auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+    if(func != nullptr)
+    {
+        func(instance, debugMessenger, pAllocator);
+    }
+}
+
 void HelloTriangleApplication::initWindow()
 {
     glfwInit();
@@ -25,6 +53,7 @@ void HelloTriangleApplication::initVulkan()
     PrintExtension();
 
     createInstance();
+    setupDebugMessenger();
 }
 
 void HelloTriangleApplication::mainLoop()
@@ -37,6 +66,11 @@ void HelloTriangleApplication::mainLoop()
 
 void HelloTriangleApplication::cleanup()
 {
+    if(enableValidationLayers)
+    {
+        //DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+    }
+
     vkDestroyInstance(instance, nullptr);
 
     glfwDestroyWindow(window);
@@ -46,6 +80,12 @@ void HelloTriangleApplication::cleanup()
 
 void HelloTriangleApplication::createInstance()
 {
+    // 检查验证层是否都可用
+    if(enableValidationLayers && !checkValidationLayerSupport())
+    {
+        throw std::runtime_error("validation layers requested, butnot available!");
+    }
+
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pApplicationName = "Hello Triangle";
@@ -58,15 +98,29 @@ void HelloTriangleApplication::createInstance()
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     createInfo.pApplicationInfo = &appInfo;
 
-    // 获取窗口扩展信息
-    uint32_t glfwExtensionCount = 0;
-    const char** glfwExtensions;
+    // 获取扩展的信息并设置扩展
+    auto extensions = getRequiredExtensions();
+    createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+    createInfo.ppEnabledExtensionNames = extensions.data();
 
-    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
 
     // 这两个成员决定了要启用的全局验证层
-    createInfo.enabledExtensionCount = glfwExtensionCount;
-    createInfo.ppEnabledExtensionNames = glfwExtensions;
+    if(enableValidationLayers)// 当开启验证层时记录相关信息,否则就将其设为0
+    {
+        createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+
+        populateDebugMessengerCreateInfo(debugCreateInfo);// 填充信息
+        // 想createInfo的pNext传递扩展
+        createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*)&debugCreateInfo;
+    }
+    else
+    {
+        createInfo.enabledLayerCount = 0;
+
+        createInfo.pNext = nullptr;
+    }
 
     // 创建实例并判断是否成功创建
     if(vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
@@ -93,3 +147,98 @@ void HelloTriangleApplication::PrintExtension()
         std::cout << "\t" << extension.extensionName << std::endl;
     }
 }
+
+bool HelloTriangleApplication::checkValidationLayerSupport()
+{
+    // 获得所有可用层的列表
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    // 检查所有可用图层列表中是否存在验证层中所涉及到的层
+    for(const char* layerName : validationLayers)
+    {
+        bool layerFound = false;
+
+        for(const auto& layerProperties : availableLayers)
+        {
+            if(strcmp(layerName, layerProperties.layerName) == 0)
+            {
+                layerFound = true;
+                break;
+            }
+        }
+
+        if(!layerFound)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+std::vector<const char*> HelloTriangleApplication::getRequiredExtensions()
+{
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    // 记录GLFW所需的扩展(这些是必须的)
+    std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+    // 如果开启验证层,则再添加验证层所需的扩展
+    if(enableValidationLayers)
+    {
+        extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        // VK_EXT_DEBUG_UTILS_EXTENSION_NAME "VK_EXT_debug_utils"
+    }
+
+    return extensions;
+}
+
+VKAPI_ATTR VkBool32 VKAPI_CALL HelloTriangleApplication::debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, 
+                                                                       VkDebugUtilsMessageTypeFlagsEXT messageType, 
+                                                                       const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, 
+                                                                       void* pUserData)
+{// 该函数以 PFN_vkDebugUtilsMessengerCallbackEXT  为原型
+    // VKAPI_ATTR 和 VKAPI_CALL 可确保函数具有正确的签名，以便 Vulkan 调用它。
+    std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+    return VK_FALSE;
+}
+
+void HelloTriangleApplication::setupDebugMessenger()
+{
+    if(!enableValidationLayers)
+        return;
+
+    VkDebugUtilsMessengerCreateInfoEXT createInfo;
+    populateDebugMessengerCreateInfo(createInfo);
+
+    if(CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS)
+    {
+        throw std::runtime_error("failed to set up debug messenger!");
+    }
+}
+
+void HelloTriangleApplication::populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo)
+{
+    createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+
+    createInfo.messageSeverity = 
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | 
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | 
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+
+    createInfo.messageType = 
+        VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | 
+        VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | 
+        VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+
+    createInfo.pfnUserCallback = debugCallback;
+}
+
